a7cf4605b90223c839542be46c89f1f6378626eb
KaupMa <100938009+KaupMa@users.noreply.github.com>
2023-03-19 19:37:22 +0200

    MariaDB

--- persister/database.js
+++ persister/database.js
@@ -1,33 +1,31 @@
-var mongoose = require('mongoose');
+var mariadb = require('mariadb');
+var dbConfig = require('../config.json').database;
 
-// @ TODO remove for production level.
-//mongoose.set('debug', true);
+var pool = mariadb.createPool({
+  host: dbConfig.host,
+  port: dbConfig.port,
+  user: dbConfig.user,
+  password: dbConfig.password,
+  database: dbConfig.database,
+  connectionLimit: 5
+});
 
 var db = function() {
   var initFlag = false;
   return {
-
     config: function(addr, dbname, opts, callback) {
       if( !initFlag ){
-        var connectUrl = 'mongodb://' + (addr ? addr : 'localhost:27017') + '/' + (dbname ? dbname : 'sbadmin');
-        mongoose.connect(connectUrl, (opts ? opts : {}));
-        //mongoose.createConnection(connectUrl, (opts ? opts : {}));
-
-        var db = mongoose.connection;
-
-        db.on('error', function(err) {
-          // Connection Error
-          console.log('Mongodb error encountered [' + err + ']');
-
-          if (callback) {
-            callback('ERR-MONGODB', 'mongodb - '+err.message);
-          }
-        });
-
-        db.once('open', function() {
-          initFlag = true;
-          if (callback) callback(null);
-        });
+        pool.getConnection()
+          .then(conn => {
+            console.log("MariaDB is connected");
+            initFlag = true;
+            conn.release();
+            if (callback) callback(null);
+          })
+          .catch(err => {
+            console.log("MariaDB error encountered [" + err + "]");
+            if (callback) callback('ERR-MARIADB', 'mariadb - '+err.message);
+          });
       } else {
         if (callback) callback(null);
       }
