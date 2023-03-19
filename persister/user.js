ff23df91da9587d75aef459e65415d004d2ec423
KaupMa <100938009+KaupMa@users.noreply.github.com>
2023-03-19 19:39:57 +0200

    MariaDB

--- persister/user.js
+++ persister/user.js
@@ -1,21 +1,72 @@
-var mongoose = require('mongoose');
+var mariadb = require('mariadb');
+var dbConfig = require('../config.json').database;
 
-/**
- * User 정보를 담기 위한 Mongo DB model
- * @name userModel
- */
+var pool = mariadb.createPool({
+  host: dbConfig.host,
+  port: dbConfig.port,
+  user: dbConfig.user,
+  password: dbConfig.password,
+  database: dbConfig.database,
+  connectionLimit: 5
+});
+
 var userModel = function () {
 
-  var userSchema = mongoose.Schema({
-    username: String,
-    password: String,
-    email: String,
-    gender: String,
-    address: String
-  });
- 
+  var User = function() {};
 
-  return mongoose.model('User', userSchema);
+  User.prototype.getAllUsers = function (callback) {
+    pool.query('SELECT * FROM users', function (err, rows) {
+      if (err) {
+        callback('ERR-MARIADB', err);
+      } else {
+        callback(null, rows);
+      }
+    });
+  };
+
+  User.prototype.getUserById = function (id, callback) {
+    pool.query('SELECT * FROM users WHERE id = ?', [id], function (err, rows) {
+      if (err) {
+        callback('ERR-MARIADB', err);
+      } else {
+        callback(null, rows[0]);
+      }
+    });
+  };
+
+  User.prototype.createUser = function (user, callback) {
+    pool.query('INSERT INTO users SET ?', user, function (err, result) {
+      if (err) {
+        callback('ERR-MARIADB', err);
+      } else {
+        user.id = result.insertId;
+        callback(null, user);
+      }
+    });
+  };
+
+  User.prototype.updateUser = function (id, user, callback) {
+    pool.query('UPDATE users SET ? WHERE id = ?', [user, id], function (err) {
+      if (err) {
+        callback('ERR-MARIADB', err);
+      } else {
+        user.id = id;
+        callback(null, user);
+      }
+    });
+  };
+
+  User.prototype.deleteUser = function (id, callback) {
+    pool.query('DELETE FROM users WHERE id = ?', [id], function (err) {
+      if (err) {
+        callback('ERR-MARIADB', err);
+      } else {
+        callback(null);
+      }
+    });
+  };
+
+  return new User();
 };
 
-module.exports = new userModel();
+module.exports = userModel();
